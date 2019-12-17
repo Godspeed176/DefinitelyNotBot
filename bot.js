c1c2d330c5cc57bf48e0598d2bce7f90b7847ed3
Godspeed176 <58958540+Godspeed176@users.noreply.github.com>
2019-12-16 19:38:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,3 +19,16 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+// Set the prefix
+const prefix = "!";
+client.on("message", (message) => {
+  // Exit and stop if it's not there
+  if (!message.content.startsWith(prefix)) return;
+ 
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
